47eec56bf4fddb5a0ed724d69a23e460f8b1ad40
alexandrehpiva <alexandrehpiva@gmail.com>
2021-05-10 13:25:37 -0300

    Improve useDocumentTitle hook

--- src/hooks/useDocumentTitle.js
+++ src/hooks/useDocumentTitle.js
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 const useDocumentTitle = message => {
   useEffect(() => {
     document.title = message;
-  });
+  }, [message]);
 };
 
 export default useDocumentTitle;
